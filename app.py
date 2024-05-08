cacc09579e107cc5b0bd1020e8d81f66e7699866
Collins Chetekei <112887160+chete-py@users.noreply.github.com>
2024-05-08 10:57:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -45,13 +45,13 @@ with tab1:
             working = (0.5/100) * value
             ex_pr += working
 
-        
         if loss_of_use == 'KES 4500':
             car_hire += 4500
-        elif loss_of_use == 'N/A':
+        elif loss_of_use == 'NA':
             car_hire += 0
         elif loss_of_use == 'KES 3000':
             car_hire += 3000
+
             
         if policy_fee == "Renewal":
             fee += 100
