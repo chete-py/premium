fb9a0fe7e25458f8a57455c373a3b0367b158e2b
Collins Chetekei <112887160+chete-py@users.noreply.github.com>
2024-05-17 23:13:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -708,7 +708,7 @@ with view2:
     
         df = pd.DataFrame(data, columns=headers)
 
-        df['Date'] = pd.to_datetime(df['Date'])
+        df['Date'] = pd.to_datetime(df['Date'], format='%d/%m/%Y %H:%M', errors='coerce')
 
         df['Renewal Month'] = df['Date'].dt.month_name()
 
