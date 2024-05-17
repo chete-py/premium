a41083389037a60e923442bd122e536a9e8e08d5
Collins Chetekei <112887160+chete-py@users.noreply.github.com>
2024-05-17 22:51:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -708,7 +708,7 @@ with view2:
     
         df = pd.DataFrame(data, columns=headers)
 
-        df['Renewal Month'] = df['Date'].dt.month()
+        df['Renewal Month'] = df['Date'].dt.month_name()
 
         # Get the unique reviewer names from the DataFrame
         unique_outcome = df['Renewal Month'].unique()
