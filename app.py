9a9fbdd260a0a3d1aa35db3de87e1e8f991e23c6
Collins Chetekei <112887160+chete-py@users.noreply.github.com>
2024-05-21 07:53:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -733,8 +733,8 @@ with view2:
             workload = final_df[final_df['Status'] == 'Pending']
             edited_df =  st.data_editor(workload, key = 'workload')
             merged = pd.concat([final_df, edited_df])
-            merged.drop_duplicates(subset=['Key'], keep='last')
-            df1 = merged.astype(str).fillna('')            
+            final = merged.drop_duplicates(subset=['Key'], keep='last')
+            df1 = final.astype(str).fillna('')            
             # Add a button to update Google Sheets with the changes
             if st.button("Update Records", key='button1'):                
                 worksheet.update([df1.columns.tolist()] + df1.values.tolist())
