0c3bdf80f502d1b1ad4034f73ac7d6ef28c5f463
Collins Chetekei <112887160+chete-py@users.noreply.github.com>
2024-05-23 13:53:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -959,7 +959,7 @@ with view2:
             merged = pd.concat([final_df, edited_df])
             finalmerged = merged.drop_duplicates(subset=['Key'], keep='last')
             descending = finalmerged.sort_values(by=['Key'], ascending=True)
-            df5 = descending.astype(str).fillna('')  
+            df6 = descending.astype(str).fillna('')  
             num = len(exits)
             st.markdown(f'Cancelled Policy: {num}')
             # Add a button to update Google Sheets with the changes
