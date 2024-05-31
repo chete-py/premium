7e939beaf120df77a75fa95ada60ece333b917d1
Collins Chetekei <112887160+chete-py@users.noreply.github.com>
2024-05-31 07:25:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -626,14 +626,14 @@ with view1:
                     table {{
                     border-collapse: collapse;
                     width: 45%;
-                    margin: 2.5px auto; /* Center the table */
-                    font-size: 10px;
+                    margin: 0.5px auto; /* Center the table */
+                    font-size: 8px;
                     font-family: Candara;
                 }}
         
                 th, td {{
                     border: 1px solid black;
-                    padding: 5px; /* Increased padding for better spacing */
+                    padding: 2.5px; /* Increased padding for better spacing */
                     text-align: left;
                 }}
         
