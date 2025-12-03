abf68aa5e60a5e37d95bfcb96b989da6de9dc034
Collins Chetekei <112887160+chete-py@users.noreply.github.com>
2025-12-03 07:45:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -282,7 +282,7 @@ with view1:
             reg = st.text_input('Enter Registration')            
             value = int(st.number_input('Sum Insured')) 
             staff = st.selectbox("Prepared By:", ["Collins Chetekei", "Daniel Cheruiyot", "Patrick Kimani", "Ephantus Ngari", "Samuel Ndoto", "Charity Rono", "Monica Waruguru"])
-            loss_of_use = st.selectbox("Choose Loss Of Use Amount charged", [3000, "Excluded"])
+            loss_of_use = st.selectbox("Choose Loss Of Use Amount charged", [3000, 6000, "Excluded"])
             windscreen = int(st.number_input('Windscreen Amount Above Free Limit'))
             span = st.selectbox("Choose Length of cover", ["Annual Cover", "Pro-Rated Cover"])
             if span == "Pro-Rated Cover":                
@@ -358,6 +358,8 @@ with view1:
                 car_hire += 0                
             elif loss_of_use == 3000:
                 car_hire += 3000
+            elif loss_of_use == 5000:
+                car_hire += 5000
             
             if value < 600000:
                 apa_rate = 'Minimum 500K'
@@ -644,7 +646,7 @@ with view1:
                         <th colspan="2"><img src="https://th.bing.com/th/id/OIP.Jz5UcTVU1JbjzmCGb2nt8gAAAA?w=194&h=186&rs=1&pid=ImgDetMain" alt="ICEA Logo"></th>
                         <th colspan="2"><img src="https://th.bing.com/th/id/OIP.snnk8ltfoo-_qalEPNgqEQHaEK?w=920&h=518&rs=1&pid=ImgDetMain" alt="OM Logo"></th> 
                         <th colspan="2"><img src="https://th.bing.com/th/id/OIP.yI9_Wi3jP0NwLeYGgbFUhQHaEK?rs=1&pid=ImgDetMain" alt="GA Logo"></th> 
-                        <th colspan="2"><img src="https://th.bing.com/th/id/OIP.IJBSY54mBVEfqRY3oQCVMgHaHa?pid=ImgDet&w=207&h=207&c=7&dpr=1.5" alt="GA Logo"></th> 
+                        # <th colspan="2"><img src="https://th.bing.com/th/id/OIP.IJBSY54mBVEfqRY3oQCVMgHaHa?pid=ImgDet&w=207&h=207&c=7&dpr=1.5" alt="GA Logo"></th> 
              
                      </tr>
                     
@@ -663,8 +665,8 @@ with view1:
                         <th style="background-color: #17B169">Premium</th>
                         <th style="background-color: #17B169">Rate</th>
                         <th style="background-color: #17B169">Premium</th>
-                        <th style="background-color: #17B169">Rate</th>
-                        <th style="background-color: #17B169">Premium</th>
+                        # <th style="background-color: #17B169">Rate</th>
+                        # <th style="background-color: #17B169">Premium</th>
                     </tr>
                                                                        
                     <tr>
@@ -682,8 +684,8 @@ with view1:
                         <td>{formatted_om_premium}</td>
                         <td style="color:red">{ga_rate}%</td>
                         <td>{formatted_ga_premium}</td>
-                        <td style="color:red">{aig_rate}</td>
-                        <td>{formatted_aig_premium}</td> 
+                        # <td style="color:red">{aig_rate}</td>
+                        # <td>{formatted_aig_premium}</td> 
                                         
                     </tr>                     
 
@@ -702,8 +704,8 @@ with view1:
                         <td>{formatted_om_ex_prt}</td>
                         <td style="color:red">0.25%</td>
                         <td>{formatted_fidelity_ex_prt}</td>
-                        <td style="color:red">0.25%</td>
-                        <td>{formatted_fidelity_ex_prt}</td>
+                        # <td style="color:red">0.25%</td>
+                        # <td>{formatted_fidelity_ex_prt}</td>
                                                     
                     </tr>           
                                                 
@@ -722,8 +724,8 @@ with view1:
                         <td>{formatted_om_pvt} </td>
                         <td style="color:red">0.25%</td>
                         <td>{formatted_fidelity_pvt}</td>
-                        <td style="color:red">0.25%</td>
-                        <td>{formatted_fidelity_pvt}</td>
+                        # <td style="color:red">0.25%</td>
+                        # <td>{formatted_fidelity_pvt}</td>
                                                               
                     </tr>              
     
@@ -742,8 +744,8 @@ with view1:
                         <td>{lou}</td>
                         <td style="color:red" >{loss_of_use}</td>
                         <td>{formatted_car_hire}</td>
-                        <td style="color:red" >4000</td>
-                        <td>4000</td>
+                        # <td style="color:red" >4000</td>
+                        # <td>4000</td>
                       
                     </tr>      
                         
@@ -763,8 +765,8 @@ with view1:
                         <td class='gross_premium'>{formatted_om_gross_premium}</td>
                         <td></td>
                         <td class='gross_premium'>{formatted_ga_gross_premium}</td>
-                        <td></td>
-                        <td class='gross_premium'>{formatted_aig_gross_premium}</td>                      
+                        # <td></td>
+                        # <td class='gross_premium'>{formatted_aig_gross_premium}</td>                      
                         
                         
                 
@@ -785,8 +787,8 @@ with view1:
                         <td >{formatted_om_levies}</td>
                         <td style="color:red">0.45%</td>
                         <td >{formatted_ga_levies}</td>
-                        <td style="color:red">0.45%</td>
-                        <td >{formatted_aig_levies}</td>                       
+                        # <td style="color:red">0.45%</td>
+                        # <td >{formatted_aig_levies}</td>                       
                 
                     </tr>
                     
@@ -805,8 +807,8 @@ with view1:
                         <td>{fee}</td>
                         <td></td>
                         <td>{fee}</td>
-                        <td></td>
-                        <td>{fee}</td>                        
+                        # <td></td>
+                        # <td>{fee}</td>                        
                                                           
                     </tr>
                     
@@ -825,8 +827,8 @@ with view1:
                         <td class = 'bold' style="color:#152637">{formatted_om_total}</td>
                         <td></td>
                         <td class = 'bold' style="color:#152637">{formatted_ga_total}</td>
-                        <td></td>
-                        <td class = 'bold' style="color:#152637">{formatted_aig_total}</td>
+                        # <td></td>
+                        # <td class = 'bold' style="color:#152637">{formatted_aig_total}</td>
                                                       
                     </tr>          
 
