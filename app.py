0bfb46ddb1987483102fd6c497ad57587a556dd8
Collins Chetekei <112887160+chete-py@users.noreply.github.com>
2026-04-19 19:19:45 +0300

    Update app.py

--- app.py
+++ app.py
@@ -472,8 +472,9 @@ with view1:
                 formatted_aig_premium = format_with_commas((value * (aig_rate/100) * (days/365)))
                 aig_gross_premium = ((value * (aig_rate/100) * (days/365)) + (max(value * 0.25/100, 5000)))
                 formatted_aig_gross_premium = format_with_commas((value * (aig_rate/100) * (days/365)) + (max(value * 0.25/100, 5000)))
+                aig_levies = (0.0045 * aig_gross_premium)
                 formatted_aig_levies = format_with_commas(0.0045 * aig_gross_premium)
-                formatted_aig_total = (formatted_aig_gross_premium + formatted_aig_levies)
+                formatted_aig_total = format_with_commas(aig_gross_premium + aig_levies)
                 formatted_aig_car_hire = 0.00
             else:
                 if value < 1000001:
@@ -781,7 +782,7 @@ with view1:
                         <td>{lou}</td>
                         <td style="color:red" >{loss_of_use}</td>
                         <td>{formatted_car_hire}</td>
-                        <td style="color:red" >'Consult team'</td>
+                        <td style="color:red" >Excluded</td>
                         <td>0.00</td>
                       
                       
