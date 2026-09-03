a0408c3df7d9c8b1cb4f9d44554a295429a57960
Collins Chetekei <112887160+chete-py@users.noreply.github.com>
2026-09-03 12:27:00 +0300

    Update app.py

--- app.py
+++ app.py
@@ -535,7 +535,7 @@ with view1:
                 elif value > 1000000 and value < 1500001 and model == "Probox, Succeed, Sienta, Noah & Voxy":
                     sanlam_rate = 7.5
                     sanlam_premium = (value * (sanlam_rate/100) * (days/365))
-                    sanlam_gross_premium = ( sanlam_premium + fidelity_pvt  car_hire)
+                    sanlam_gross_premium = ( sanlam_premium + fidelity_pvt + car_hire)
                 elif value > 1500000 and value < 2500001 and model != "Probox, Succeed, Sienta, Noah & Voxy":
                     sanlam_rate = 4.25
                     sanlam_premium = (value * (sanlam_rate/100) * (days/365)) 
