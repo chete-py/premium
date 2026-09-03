a99d8fcd01d8240a17d676f5313b35b8220c24a3
Collins Chetekei <112887160+chete-py@users.noreply.github.com>
2026-09-03 12:05:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -584,7 +584,7 @@ with view1:
 
                 
                 formatted_icea_premium = format_with_commas(icea_premium)
-                formatted_ga_premium = format_with_commas(sanlam_premium)
+                formatted_sanlam_premium = format_with_commas(sanlam_premium)
                 # formatted_om_premium = format_with_commas(om_premium)
                 formatted_fidelity_premium = format_with_commas(fidelity_premium)
                 
